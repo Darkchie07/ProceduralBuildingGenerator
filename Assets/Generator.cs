02196f4b0484b5a5ae126ea78da132a3041ca484
Augusta Steven Benedict <steven02asb@gmail.com>
2023-09-29 18:19:13 +0700

    Add Three Shape

--- Assets/Generator.cs
+++ Assets/Generator.cs
@@ -13,14 +13,18 @@ public class Generator : MonoBehaviour
     public int jumlahLantai;*/
     private Mesh mesh;
 
+    private void Start()
+    {
+        CreateRCBuilding();
+    }
+
     public void CreateCube(float _lenght = 0f, float _width = 0f, float _height = 0f, float _dasar = 0f)
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
         
         MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
-        meshCollider.sharedMesh = mesh;
-        
+
         float tempLenght = _lenght / 2;
         float tempWidth = _width / 2;
         // Define the vertices of the cube
@@ -82,6 +86,7 @@ public class Generator : MonoBehaviour
 
         // Optionally, calculate normals
         mesh.RecalculateNormals();
+        meshCollider.sharedMesh = mesh;
     }
     
     public void CreatePyramid(float _lenght = 0f, float _width = 0f, float _height = 0f, float _dasar = 0f)
@@ -145,5 +150,253 @@ public class Generator : MonoBehaviour
 
         // Optionally, calculate normals
         mesh.RecalculateNormals();
+        meshCollider.sharedMesh = mesh;
+    }
+    
+    public void CreateUBuilding(float _lenght = 0f, float _width = 0f, float _height = 0f, float _dasar = 0f)
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+
+        // Define the vertices of the cube
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(2, 0, 2), //0 
+            new Vector3(1, 0, 2), //1 
+            new Vector3(1, 0, 0), //2 
+            new Vector3(-1, 0, 0), //3 
+            new Vector3(-1, 0, 2), //4 
+            new Vector3(-2, 0, 2), //5
+            new Vector3(-2, 0, -2), //6
+            new Vector3(2, 0, -2), //7 
+            new Vector3(1, 0, -2), //8
+            new Vector3(-1, 0, -2), //9
+            
+            new Vector3(2, 2, 2), //10
+            new Vector3(1, 2, 2), //11 
+            new Vector3(1, 2, 0), //12 
+            new Vector3(-1, 2, 0), //13
+            new Vector3(-1, 2, 2), //14
+            new Vector3(-2, 2, 2), //15 
+            new Vector3(-2, 2, -2), //16
+            new Vector3(2, 2, -2), //17
+            new Vector3(1, 2, -2), //18
+            new Vector3(-1, 2, -2), //19
+        };
+
+        // Define the triangles to form the cube's faces
+        int[] triangles = new int[]
+        {
+            0, 1, 8,
+            0, 8, 7,
+            4, 5, 6,
+            4, 6, 9,
+            2, 3, 9,
+            2, 9, 8,
+            
+            10, 18, 11,
+            10, 17, 18,
+            14, 16, 15,
+            14, 19, 16,
+            12, 19, 13,
+            12, 18, 19,
+            
+            0, 11, 1,
+            0, 10, 11,
+            
+            1, 12, 2,
+            1, 11, 12,
+            
+            3, 14, 4,
+            3, 13, 14,
+            
+            2, 13, 3,
+            2, 12, 13,
+            
+            4, 15, 5,
+            4, 14, 15,
+            
+            0, 7, 17,
+            0, 17, 10,
+            
+            5, 16, 6,
+            5, 15, 16,
+            
+            6, 17, 7,
+            6, 16, 17,
+        };
+
+        // Define UV coordinates (the size matches the number of vertices)
+        Vector2[] uv = new Vector2[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            uv[i] = new Vector2(vertices[i].x, vertices[i].y); // Simple UV mapping based on vertex positions
+        }
+
+        // Assign vertices, triangles, and UV coordinates to the mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uv;
+
+        // Optionally, calculate normals
+        mesh.RecalculateNormals();
+        meshCollider.sharedMesh = mesh;
+    }
+    
+    public void CreateLBuilding(float _lenght = 0f, float _width = 0f, float _height = 0f, float _dasar = 0f)
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+
+        // Define the vertices of the cube
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(-2, 0, 2), //0 
+            new Vector3(0, 0, 2), //1 
+            new Vector3(0, 0, 0), //2 -
+            new Vector3(2, 0, 0), //3 -
+            new Vector3(2, 0, -2), //4 
+            new Vector3(-2, 0, -2), //5
+            new Vector3(-2, 0, 0), //6 
+
+            new Vector3(-2, 2, 2), //7 
+            new Vector3(0, 2, 2), //8 
+            new Vector3(0, 2, 0), //9 -
+            new Vector3(2, 2, 0), //10 -
+            new Vector3(2, 2, -2), //11 
+            new Vector3(-2, 2, -2), //12
+            new Vector3(-2, 2, 0), //13
+        };
+
+        // Define the triangles to form the cube's faces
+        int[] triangles = new int[]
+        {
+            6, 1, 0,
+            2, 1, 6,
+            
+            4, 3, 6,
+            5, 4, 6,
+            
+            8, 13, 7,
+            8, 9, 13,
+            
+            10, 11, 13,
+            11, 12, 13,
+            
+            0, 1, 8,
+            0, 8, 7,
+            
+            1, 2, 9,
+            1, 9, 8,
+            
+            2, 3, 9,
+            3, 10, 9,
+            
+            3, 4, 10,
+            11, 10, 4,
+            
+            4, 5, 12,
+            12, 11, 4,
+            
+            0, 7, 5,
+            7, 12, 5,
+        };
+
+        // Define UV coordinates (the size matches the number of vertices)
+        Vector2[] uv = new Vector2[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            uv[i] = new Vector2(vertices[i].x, vertices[i].y); // Simple UV mapping based on vertex positions
+        }
+
+        // Assign vertices, triangles, and UV coordinates to the mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uv;
+
+        // Optionally, calculate normals
+        mesh.RecalculateNormals();
+        meshCollider.sharedMesh = mesh;
+    }
+    
+    public void CreateRCBuilding(float _lenght = 0f, float _width = 0f, float _height = 0f, float _dasar = 0f)
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        
+        MeshCollider meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = mesh;
+
+        // Define the vertices of the cube
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0, 0, 2), //0
+            new Vector3(2, 0, 2), //1 
+            new Vector3(2, 0, -2), //2
+            new Vector3(-2, 0, -2), //3 
+            new Vector3(-2, 0, 0), //4
+            new Vector3(0, 0, 0), //5
+            
+            new Vector3(0, 2, 2), //6
+            new Vector3(2, 2, 2), //7 
+            new Vector3(2, 2, -2), //8
+            new Vector3(-2, 2, -2), //9 
+            new Vector3(-2, 2, 0), //10
+            new Vector3(0, 2, 0), //11
+        };
+
+        // Define the triangles to form the cube's faces
+        int[] triangles = new int[]
+        {
+            0, 4, 5,
+            1, 3, 2,
+            0, 5, 1,
+            5, 4, 3,
+            
+            6, 11, 10,
+            7, 8, 9,
+            6, 7, 11,
+            11, 9, 10,
+            
+            0, 10, 4,
+            0, 6, 10,
+            
+            0, 1 , 7,
+            0, 7, 6,
+            
+            1, 2, 8,
+            1, 8, 7,
+            
+            2, 3, 9,
+            2, 9, 8,
+            
+            3, 4, 10,
+            3, 10, 9,
+        };
+
+        // Define UV coordinates (the size matches the number of vertices)
+        Vector2[] uv = new Vector2[vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            uv[i] = new Vector2(vertices[i].x, vertices[i].y); // Simple UV mapping based on vertex positions
+        }
+
+        // Assign vertices, triangles, and UV coordinates to the mesh
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.uv = uv;
+
+        // Optionally, calculate normals
+        mesh.RecalculateNormals();
+        meshCollider.sharedMesh = mesh;
     }
 }
